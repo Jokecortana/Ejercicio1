fed1dd93b3a2917c486b4ed2d532e21b9285ff87
jokecortana <jokecortana@gmail.com>
2023-04-30 14:07:34 -0600

    Se agrega clase alumno

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+
+    private Editnom, Editap
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+
+    }
+
+    fun click(view: View) {
+
+        Toast.makeText(this, "Click en boton", Toast.LENGTH_LONG).show()
+
+
     }
 }
